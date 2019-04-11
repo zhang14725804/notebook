b782b6acad9ef6bb44e4c56ed08044cfaca056a6
zhang14725804 <978817036@qq.com>
2019-04-11 10:16:18 +0800

    删除未完成的项目

--- react-vue/react-chat/src.2/index.js
+++ react-vue/react-chat/src.2/index.js
@@ -20,20 +20,20 @@ ReactDOM.render((
   </Provider>),document.getElementById('root')
 )
 
-//两层箭头函数
-const add=x=>y=>x+y+3
-const res=add(3)(2)
-console.log("res::"+res)
-//遍历对象key
-const tObject={name:'bigo',age:'woniu'}
-console.log(Object.keys(tObject))
-//遍历对象key
+// //两层箭头函数
+// const add=x=>y=>x+y+3
+// const res=add(3)(2)
+// console.log("res::"+res)
+// //遍历对象key
+// const tObject={name:'bigo',age:'woniu'}
+// console.log(Object.keys(tObject))
+// //遍历对象key
 
-for(let k in tObject){
-  console.log(k)
-}
-//...展开符号 (返回数组)
-function cArgs(...args){
-  console.log(args)
-}
-cArgs('React','Vue','webpack')
+// for(let k in tObject){
+//   console.log(k)
+// }
+// //...展开符号 (返回数组)
+// function cArgs(...args){
+//   console.log(args)
+// }
+// cArgs('React','Vue','webpack')
--- react-vue/react-eat/src/components/toast/ToastUtil.js
+++
@@ -1,9 +0,0 @@
-import Toast from './Toast'
-
-var ToastUtil = {
-    show: function (str) {
-        new Toast().show(str);
-    }
-}
-
-export default ToastUtil;
