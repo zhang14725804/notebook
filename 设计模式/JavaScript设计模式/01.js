ebfbb8c4072e5a9b0f6806e187c3199577941649
zhang14725804 <978817036@qq.com>
2020-05-11 19:20:32 +0800

    javascript函数

--- 设计模式/JavaScript设计模式/01.js
+++ 设计模式/JavaScript设计模式/01.js
@@ -14,4 +14,117 @@ var checkPassword = function(){}
 // 用对象收编变量
 var checkObject = {
     checkName:function(){},
+    checkEmail:function(){},
+    checkPassword:function(){},
 }
+
+// 用对象收编变量的另一种形式
+var checkObject = function(){}
+checkObject.checkName = function(){}
+checkObject.checkEmail = function(){}
+checkObject.checkPassword = function(){}
+
+// 使对象可以复制，新创建的对象同样继承这些方法
+var CheckObject = function(){
+    return {
+        checkName:function(){},
+        checkEmail:function(){},
+        checkPassword:function(){},
+    }
+}
+var a = CheckObject()
+a.checkName()
+
+// 类的创建方式（缺点：没有共享属性和方法）
+var CheckObject = function(){
+    this.checkName = function(){}
+    this.checkEmail = function(){}
+    this.checkPassword = function(){}
+}
+var a = new CheckObject()
+a.checkEmail()
+
+// 共享方法和属性
+var CheckObject = function(){}
+CheckObject.prototype.checkName = function(){}
+CheckObject.prototype.checkEmail = function(){}
+CheckObject.prototype.checkPassword = function(){}
+var a = checkObject()
+a.checkName()
+
+// 共享方法和属性（重写原型对象的方式）
+var CheckObject = function(){}
+CheckObject.prototype = {
+    checkName:function(){},
+    checkEmail:function(){},
+    checkPassword:function(){},
+}
+var a = new CheckObject()
+a.checkName()
+a.checkEmail()
+a.checkPassword()
+
+// 链式调用
+var checkObject = {
+    checkName:function(){
+        return this
+    },
+    checkEmail:function(){
+        return this
+    },
+    checkPassword:function(){
+        return this
+    },
+}
+checkObject.checkName().checkEmail().checkPassword()
+// 或者
+var CheckObject = function(){}
+CheckObject.prototype = {
+    checkName:function(){
+        return this
+    },
+    checkEmail:function(){
+        return this
+    },
+    checkPassword:function(){
+        return this
+    },
+}
+var a = new CheckObject()
+a.checkName().checkEmail().checkPassword()
+
+
+// 添加方法的功能方法（避免污染）
+Function.prototype.addMethod = function(name,fn){
+    this[name] = fn
+}
+var methods = function(){}
+methods.addMethod("checkName",function(){})
+methods.checkName()
+
+// 继续链式添加方法
+Function.prototype.addMethod = function(name,fn){
+    this[name] = fn
+    return this
+}
+var methods = function(){}
+methods.addMethod("checkName",function(){}).addMethod("checkEmail",function(){})
+
+// 继续链式使用（函数式调用方式）
+var methods = function(){}
+methods.addMethod("checkName",function(){
+    return this
+}).addMethod("checkEmail",function(){
+    return this
+})
+methods.checkName().checkEmail()
+
+// 类试调用方式
+Function.prototype.addMethod = function(name,fn){
+    this.prototype[name] = fn
+    return this
+}
+var methods = function(){}
+methods.addMethod("checkName",function(){}).addMethod("checkEmail",function(){})
+var m = new methods()
+m.checkName()
