18c7d345b3e69b6daa6da0cd78f694c01fc1dd0f
zhang14725804 <978817036@qq.com>
2020-05-11 10:28:37 +0800

    javascript设计模式

---
+++ 设计模式/JavaScript设计模式/01.js
@@ -0,0 +1,17 @@
+/*
+    从表单验证功能进入JavaScript
+*/
+
+// 基础版（到处都是全局变量）
+function checkName(){}
+function checkEmail(){}
+function checkPassword(){}
+
+var checkName = function(){}
+var checkEmail = function(){}
+var checkPassword = function(){}
+
+// 用对象收编变量
+var checkObject = {
+    checkName:function(){},
+}
