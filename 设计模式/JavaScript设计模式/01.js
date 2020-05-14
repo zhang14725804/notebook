291620cb4578f105cdff6ad093f16febafa4875d
zhang14725804 <978817036@qq.com>
2020-05-14 19:49:46 +0800

    javascript入门

--- 设计模式/JavaScript设计模式/01.js
+++ 设计模式/JavaScript设计模式/01.js
@@ -1,8 +1,6 @@
 /*
-    从表单验证功能进入JavaScript
+    全局变量陷阱
 */
-
-// 基础版（到处都是全局变量）
 function checkName(){}
 function checkEmail(){}
 function checkPassword(){}
@@ -11,20 +9,24 @@ var checkName = function(){}
 var checkEmail = function(){}
 var checkPassword = function(){}
 
-// 用对象收编变量
+/*
+    用对象收编变量（减少全局变量）
+*/
 var checkObject = {
     checkName:function(){},
     checkEmail:function(){},
     checkPassword:function(){},
 }
 
-// 用对象收编变量的另一种形式
 var checkObject = function(){}
 checkObject.checkName = function(){}
 checkObject.checkEmail = function(){}
 checkObject.checkPassword = function(){}
 
-// 使对象可以复制，新创建的对象同样继承这些方法
+
+/*
+    使对象可以复制，新创建的对象同样继承这些方法
+*/ 
 var CheckObject = function(){
     return {
         checkName:function(){},
@@ -32,10 +34,13 @@ var CheckObject = function(){
         checkPassword:function(){},
     }
 }
-var a = CheckObject()
+var a = CheckObject() // a和CheckObject没有任何关系
 a.checkName()
 
-// 类的创建方式（缺点：没有共享属性和方法）
+
+/*
+    类的创建方式（缺点：没有共享属性和方法）
+*/ 
 var CheckObject = function(){
     this.checkName = function(){}
     this.checkEmail = function(){}
@@ -44,15 +49,20 @@ var CheckObject = function(){
 var a = new CheckObject()
 a.checkEmail()
 
-// 共享方法和属性
+
+/*
+    共享方法和属性
+*/ 
 var CheckObject = function(){}
 CheckObject.prototype.checkName = function(){}
 CheckObject.prototype.checkEmail = function(){}
 CheckObject.prototype.checkPassword = function(){}
-var a = checkObject()
+var a = new checkObject()
 a.checkName()
 
-// 共享方法和属性（重写原型对象的方式）
+/*
+    共享方法和属性（重写原型对象的方式）
+*/ 
 var CheckObject = function(){}
 CheckObject.prototype = {
     checkName:function(){},
@@ -61,10 +71,10 @@ CheckObject.prototype = {
 }
 var a = new CheckObject()
 a.checkName()
-a.checkEmail()
-a.checkPassword()
 
-// 链式调用
+/*
+    链式调用
+*/ 
 var checkObject = {
     checkName:function(){
         return this
@@ -94,7 +104,9 @@ var a = new CheckObject()
 a.checkName().checkEmail().checkPassword()
 
 
-// 添加方法的功能方法（避免污染）
+/*
+    添加方法的功能方法（避免污染）
+*/ 
 Function.prototype.addMethod = function(name,fn){
     this[name] = fn
 }
@@ -110,7 +122,10 @@ Function.prototype.addMethod = function(name,fn){
 var methods = function(){}
 methods.addMethod("checkName",function(){}).addMethod("checkEmail",function(){})
 
-// 继续链式使用（函数式调用方式）
+
+/*
+    继续链式使用（函数式调用方式）
+*/ 
 var methods = function(){}
 methods.addMethod("checkName",function(){
     return this
@@ -119,7 +134,9 @@ methods.addMethod("checkName",function(){
 })
 methods.checkName().checkEmail()
 
-// 类试调用方式
+/*
+    类试调用方式，使用new创建对象
+*/ 
 Function.prototype.addMethod = function(name,fn){
     this.prototype[name] = fn
     return this
